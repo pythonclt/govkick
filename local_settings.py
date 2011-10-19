e19659dd75a84444cc04ef35b851532b9885f925
Matt Johnson <johnson.matthew.h@gmail.com>
2011-10-18 20:50:34 -0400

    edited urls.py

--- local_settings.py
+++ local_settings.py
@@ -1,3 +1,24 @@
+import os.path
+import django.conf.global_settings as DEFAULT_SETTINGS
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+ROOT_DIR = os.path.split(PROJECT_DIR)[0]
+
+
+STATIC_ROOT = ROOT_DIR + '/static/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = ROOT_DIR + '/media/'
+MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+ADMINS = ()
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -8,3 +29,4 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+                                       )
--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,3 +19,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+
