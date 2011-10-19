2c6fe4eb66a529d016f4f7fba767ea264c702a8f
Matt Johnson <johnson.matthew.h@gmail.com>
2011-10-19 01:16:04 -0400

    edited some stuff

--- main/views.py
+++ main/views.py
@@ -1,7 +1,1 @@
-from django.shortcuts import render_to_response, redirect
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import RequestContext, Context
 
-def home(request):
-    page_title = "Development Testing"
-    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
