61df7e2f2c25d6b74209da3283277535ce4ea0e6
Matt Johnson <johnson.matthew.h@gmail.com>
2011-10-18 22:49:41 -0400

    updated views.py with render instead of render_to_response

--- views.py
+++ views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import RequestContext, Context
 
 def home(request):
     page_title = "Development Testing"
-    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
+    return render(request, 'index.html', locals())
