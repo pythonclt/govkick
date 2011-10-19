636a2af1da2731306638838bc6bf049568403fde
Matt Johnson <johnson.matthew.h@gmail.com>
2011-10-19 01:46:54 -0400

    made things more pretty... I'm going to bed now

--- views.py
+++ views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 def home(request):
-    page_title = "Development Testing"
+    ### START html header variables ###
+    page_title = "Home"
+    meta_keywords = "Govkick, government, home, signup, sign-up"
+    meta_description = "Home page for Govkick.com"
+    ### END html header variables ###
+
+    random_message = "Development testing"
     return render(request, 'index.html', locals())
